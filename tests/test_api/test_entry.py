f2e2d6394f18a5eb6e76ea832cd6304e4f4c5311
avpps <avppsdev@gmail.com>
2023-12-29 23:59:07 +0100

    api.entry test wip

--- tests/test_api/test_entry.py
+++ tests/test_api/test_entry.py
@@ -1,5 +1,7 @@
-import unittest
+import unittest.mock
 from typing import Tuple
+
+from pymongo.errors import InvalidURI
 
 from tests.test_api.integration import IntegrationSetUp
 
@@ -9,6 +11,8 @@ class TestEntry(IntegrationSetUp):
     def api_path(self) -> Tuple[str, str]:
         return "sources.api.entry_", "entry"
 
-    @unittest.expectedFailure
+    @unittest.mock.patch("builtins.open", unittest.mock.MagicMock())
     def test_entry(self):
-        self.api_func()
+        with self.assertRaises(InvalidURI):
+            self.api_func()
+        self.assertEqual(open.call_count, 1)
